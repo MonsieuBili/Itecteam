070d8c13cfc6f6df67ea9152ebc421b5873c9347
Chocolate529 <lupse.ioan.victor@gmail.com>
2023-04-01 18:49:28 +0300

    1

---
+++ api.js
@@ -0,0 +1,28 @@
+async function postData(url = "", data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer 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"
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+  postData("https://api.openai.com/v1/chat/completions", {
+      "model": "gpt-3.5-turbo",
+      "messages": [{"role": "user", "content": "Hello! Can you write me a poem about cats in spanish!"}]
+    }).then((data) => {
+        console.log(data)
+    var poem = data.choices[0].message.content
+    console.log(poem)
+
+  });
